1d62c0f5cc7c9a48a0917bfbd303b69df128273f
ddkogit <git config --global user.email>
2024-03-18 09:46:11 +0545

    login logout done

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,49 @@ import Login from "./Pages/Login/Login";
 
 import { url } from "./Data";
 import Button from "./Components/Button/Button";
+import axios from "axios";
 function App() {
-  const [token, setToken] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
 
   const handleLogin = (email, password) => {
-    console.log(email, password);
+
+    let data = {
+      "emailOrPhone": email,
+      "password": password,
+    };
+   
+
+
+axios.post(`${url}/api/v1/admin/user/login`, data)
+    .then((response) => {
+        console.log('Login successful:', response.data.data.accessToken);
+setAccessToken(response.data.data.accessToken);
+    })
+    .catch((error) => {
+        console.error('Error:', error.message);
+        if (error.response) {
+            console.error('Status code:', error.response.status);
+            console.error('Response data:', error.response.data);
+        }
+    });
+
+
+
   };
 
-  const handleLogout =()=>{
-    
-  }
+  const handleLogout = () => {
+    axios.get(`${url}/api/v1/admin/user/logout`).then((res) => {
+      console.log(res.data);
+    });
 
-  if (!token) {
+    setAccessToken(null);
+
+  };
+
+  if (!accessToken) {
     return (
       <>
         <Login handleLogin={handleLogin} />
-
-        
       </>
     );
   }
