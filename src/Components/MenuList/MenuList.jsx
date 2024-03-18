c3b33d705dfdead776d075f282fd461785108fe9
ddkogit <git config --global user.email>
2024-03-18 17:55:06 +0545

    Menu hide and show done

--- src/Components/MenuList/MenuList.jsx
+++ src/Components/MenuList/MenuList.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MenuList.css";
-function MenuList() {
-  return <div>MenuList</div>;
+function MenuList({mouseStatus}) {
+  return (
+    <div>
+      <div  
+      className = {mouseStatus?"menu-container hidden":"menu-container"}
+      >
+        <ul className="menu-items">
+          <li>My Profile</li>
+          <li>Users</li>
+          <li>asdf</li>
+          <li>Logout</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default MenuList;
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
+import MenuList from "../MenuList/MenuList";
 
 function NavBar({ accessToken }) {
+  const [mouseStatus,setMouseStatus] = useState(true);
   return (
-    <div className="nav-container">
-      <div className="nav-logo">FIXGO</div>
+    <div>
+      <div className="nav-container">
+        <div className="nav-logo">FIXGO</div>
 
-      {accessToken && (
-        <div className="nav-menu">
-          <button className="nav-profile"></button>
-        </div>
-      )}
+          <div className="nav-menu">
+        {accessToken && (
+            <button 
+            onClick={()=>setMouseStatus(!mouseStatus)}
+            // onMouseLeave={()=>setMouseStatus(!mouseStatus)}
+            className="nav-profile"></button>
+            )}
+            </div>
+
+      </div>
+        <MenuList  mouseStatus={mouseStatus}/>
     </div>
   );
 }
