d5d1bc9aa99ff9fa8cc624c68730ca6eb56af91d
ddkogit <git config --global user.email>
2024-03-18 10:59:55 +0545

    Logout done

--- src/App.jsx
+++ src/App.jsx
@@ -12,38 +12,64 @@ function App() {
   const [accessToken, setAccessToken] = useState(null);
 
   const handleLogin = (email, password) => {
-
     let data = {
-      "emailOrPhone": email,
-      "password": password,
+      emailOrPhone: email,
+      password: password,
     };
-   
 
-
-axios.post(`${url}/api/v1/admin/user/login`, data)
-    .then((response) => {
-        console.log('Login successful:', response.data.data.accessToken);
-setAccessToken(response.data.data.accessToken);
-    })
-    .catch((error) => {
-        console.error('Error:', error.message);
+    axios
+      .post(`${url}/api/v1/admin/user/login`, data)
+      .then((response) => {
+        const accessToken = response.data.data.accessToken;
+        // setAccessToken(response.data.data.accessToken);
+        localStorage.setItem("accessToken", accessToken);
+        setAccessToken(accessToken);
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
         if (error.response) {
-            console.error('Status code:', error.response.status);
-            console.error('Response data:', error.response.data);
+          console.error("Status code:", error.response.status);
+          console.error("Response data:", error.response.data);
         }
-    });
-
-
-
+      });
   };
 
   const handleLogout = () => {
-    axios.get(`${url}/api/v1/admin/user/logout`).then((res) => {
-      console.log(res.data);
-    });
+    const token = localStorage.getItem("accessToken");
 
+    console.log(token)
+
+    localStorage.clear();
     setAccessToken(null);
 
+
+    // axios
+    //   .get(`${url}/api/v1/admin/user/logout`, {
+    //     headers: {
+    //       Authorization: `Bearer ${token}`,
+    //     },
+    //   })
+    //   .then((response) => {
+    //     console.log("Logout successful:", response.data);
+    //     localStorage.clear();
+    //     setAccessToken(null);
+    //   })
+    //   .catch((error) => {
+    //     console.error("Logout failed:", error.message);
+    //     if (error.response) {
+    //       console.error("Status code:", error.response.status);
+    //       console.error("Response data:", error.response.data);
+    //     }
+    //   });
+
+    //   axios
+    //     .get(`${url}/api/v1/admin/user/logout`)
+    //     .then((res) => {
+    //       console.log(res.data);
+    //     })
+    //     .catch((error) => console.log("logout failed", error.message));
+
+    //   setAccessToken(null);
   };
 
   if (!accessToken) {
