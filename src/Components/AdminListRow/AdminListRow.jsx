bcda9b1532ba588fbcdfc816401b2222b512723c
ddkogit <git config --global user.email>
2024-03-19 09:01:53 +0545

    admin list row created

--- src/Components/AdminListRow/AdminListRow.jsx
+++ src/Components/AdminListRow/AdminListRow.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import "./AdminListRow.css";
-function AdminListRow() {
-  return <div>AdminListRow</div>;
+function AdminListRow({sn,name,email,role,joindate,action,bold}) {
+  return <div className={bold?"adminlistrow-container bold":"adminlistrow-container"}>
+
+    <span>
+      {sn}      
+    </span>
+    <span>
+      {name}      
+    </span>
+    <span>
+      {email}      
+    </span>
+    <span>
+      {role}      
+    </span>
+    <span>
+      {joindate}      
+    </span>
+    <span>
+      {action}      
+    </span>
+
+  </div>;
 }
 
 export default AdminListRow;
--- src/Pages/AdminList/AdminList.jsx
+++ src/Pages/AdminList/AdminList.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./AdminList.css";
 import { useNavigate } from "react-router-dom";
+import Button from "../../Components/Button/Button";
+import AdminListRow from "../../Components/AdminListRow/AdminListRow";
 
 function AdminList() {
   const navigate = useNavigate();
@@ -12,10 +14,37 @@ function AdminList() {
     <div>
       <div>
         <button
-        
-        style={{
-          cursor:"pointer"
-        }}onClick={goBack}>Go Back</button>
+          style={{
+            cursor: "pointer",
+          }}
+          onClick={goBack}
+        >
+          Go Back
+        </button>
+      </div>
+      <div>
+        <div className="adminlist-top">
+          <span>Users</span>
+          <Button text={"Add User"} />
+        </div>
+        <div className="adminlist-bottom">
+          <AdminListRow
+            sn="SN."
+            name="Name"
+            email="Email"
+            joindate="Join date"
+            action="Action"
+            bold={true}
+          />
+          <AdminListRow
+            sn="SN."
+            name="Name"
+            email="Email"
+            joindate="Join date"
+            action="Action"
+            
+          />
+        </div>
       </div>
     </div>
   );
